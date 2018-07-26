6386b2a965bbb0790eedbaa91ab37e5308c29183
Brikend Behrami <brikendbehrami@gmail.com>
2018-07-26 11:54:04 -0400

    add constructor with spaceship attributes

--- spaceship.js
+++ spaceship.js
@@ -1,4 +1,16 @@
 export default class Spaceship {
-
+  constructor() {
+    this.color: "black";
+    this.width: 8;
+    this.height: 22;
+    this.position: {
+      x: 0,
+      y: 0
+    },
+    this.angle: 0;
+    this.thrusterOn: false;
+    this.rotatingClockwise: false;
+    this.rotatingCounterClockwise: false;
+  }
 
 }
